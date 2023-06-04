077b4c2c7d6f11beb82327c3a6db93a94aef5584
Meliosssss <phamhongquan19122001@gmail.com>
2023-06-04 20:01:58 +0700

    first commit

---
+++ src/main/java/fit/hutech/phamhongquan/BtktgkApplication.java
@@ -0,0 +1,13 @@
+package fit.hutech.phamhongquan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtktgkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtktgkApplication.class, args);
+	}
+
+}
---
+++ src/main/java/fit/hutech/phamhongquan/RoleRepository.java
@@ -0,0 +1,10 @@
+package fit.hutech.phamhongquan;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Map;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
